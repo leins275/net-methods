c2673bb694691488f106f4d63cbd25dd3aa9f0d2
leins275 <feitrich@yandex.ru>
2019-09-13 16:43:23 +0300

    Fixed FDM

--- Single_FD_FE/main.py
+++ Single_FD_FE/main.py
@@ -2,39 +2,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def func(x):
+    return x * (1 - x)
+
+
 def create_uniform_net(a, b, n):
     h = (b - a) / n
     net = []
 
-    for i in range(n + 1):
+    for i in range(n):
         net.append(a + i * h)
 
     return net
 
 
 def tridiagonal_matrix_alg(n, b, c, d, r, u0, un):
-    u = [u0]
+    u = [0] * n
+    beta = []
+    alpha = []
 
-    delta_i = - d[0] / c[0]
-    lambda_i = r[0] / c[0]
+    # setting up alpha and beta
+    for i in range(n):
+        if i == 0:
+            new_alpha = r[0] / c[0]
+            new_beta = - d[0] / c[0]
+        else:
+            new_alpha = (- d[i] / (c[i] + b[i] * beta[i - 1]))
+            new_beta = ((r[i] - b[i] * alpha[i - 1]) / (c[i] + b[i] * beta[i - 1]))
+        alpha.append(new_alpha)
+        beta.append(new_beta)
 
-    for i in range(1, n):
-        ui = (u[i - 1] - (r[i] - b[i] * lambda_i) / (c[i] + b[i] * delta_i)) * (c[i] - b[i] * delta_i) / d[i]
-        if i != n - 1:
-            lambda_i = (r[i] - b[i] * lambda_i) / (c[i] + b[i] * delta_i)
-            delta_i = - d[i] / (c[i] + b[i] * delta_i)
-        u.append(ui)
+    # counting u values
+    for i in range(n - 1, 0, -1):
+        if i == n - 1:
+            u[i] = un
+            # u[i] = (r[i] - b[i] * beta[i - 1]) / (c[i] + b[i] * alpha[i - 1])
+        else:
+            u[i] = alpha[i] * u[i + 1] + beta[i]
 
-    u.append(un)  # (r[n] - b[n] * lambda_i) / (c[n] + b[n] * delta_i)
+    u[0] = 0
     return u
 
 
-def fdm(n, u0, un):
-    b = [1] * n
-    c = [-2] * n
-    d = [1] * n
-    r = [-2] * n
+def fdm(n, u0, un, h):
+    b = [-1] * n
+    c = [2] * n
+    d = [-1] * n
+    r = [2 * h ** 2] * n
 
+    r[0] = r[n - 1] = 0
     u = tridiagonal_matrix_alg(n, b, c, d, r, u0, un)
 
     return u
@@ -44,6 +60,9 @@ if __name__ == '__main__':
     a = 0
     l = 1
     n = 10
-    # x = create_uniform_net(a, l, n)
-    u = fdm(n, 0, 0)
-    print(u)
+    h = (l - a) / n
+    x = create_uniform_net(a, l, n)
+    u = fdm(n, 0, 0, h)
+    u.reverse()
+    plt.plot(x, u)
+    plt.show()
