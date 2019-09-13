c0bb01c6a92e18b6635d442feb44b7f4809dcd68
leins275 <feitrich@yandex.ru>
2019-09-13 19:53:21 +0300

    Fixed FDM

--- Single_FD_FE/main.py
+++ Single_FD_FE/main.py
@@ -9,11 +9,13 @@ def func(x):
 def create_uniform_net(a, b, n):
     h = (b - a) / n
     net = []
+    f = []
 
-    for i in range(n):
-        net.append(a + i * h)
-
-    return net
+    for i in range(n + 1):
+        new_p = a + i * h
+        net.append(new_p)
+        f.append(func(new_p))
+    return net, f
 
 
 def tridiagonal_matrix_alg(n, b, c, d, r, u0, un):
@@ -27,8 +29,8 @@ def tridiagonal_matrix_alg(n, b, c, d, r, u0, un):
             new_alpha = r[0] / c[0]
             new_beta = - d[0] / c[0]
         else:
-            new_alpha = (- d[i] / (c[i] + b[i] * beta[i - 1]))
-            new_beta = ((r[i] - b[i] * alpha[i - 1]) / (c[i] + b[i] * beta[i - 1]))
+            new_alpha = (- d[i] / (c[i] + b[i] * alpha[i - 1]))
+            new_beta = ((r[i] - b[i] * beta[i - 1]) / (c[i] + b[i] * alpha[i - 1]))
         alpha.append(new_alpha)
         beta.append(new_beta)
 
@@ -40,7 +42,6 @@ def tridiagonal_matrix_alg(n, b, c, d, r, u0, un):
         else:
             u[i] = alpha[i] * u[i + 1] + beta[i]
 
-    u[0] = 0
     return u
 
 
@@ -50,19 +51,21 @@ def fdm(n, u0, un, h):
     d = [-1] * n
     r = [2 * h ** 2] * n
 
-    r[0] = r[n - 1] = 0
-    u = tridiagonal_matrix_alg(n, b, c, d, r, u0, un)
-
-    return u
+    return tridiagonal_matrix_alg(n, b, c, d, r, u0, un)
 
 
 if __name__ == '__main__':
     a = 0
     l = 1
-    n = 10
+    n = 11
     h = (l - a) / n
-    x = create_uniform_net(a, l, n)
+    x, y = create_uniform_net(a, l, n - 1)
     u = fdm(n, 0, 0, h)
-    u.reverse()
-    plt.plot(x, u)
+    # u.reverse()
+
+    plt.figure()
+    plt.plot(x, y, 'r*')
+    plt.plot(x, u, 'b--')
+    plt.plot(x, u, 'bd')
+    plt.grid()
     plt.show()
