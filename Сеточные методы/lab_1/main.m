538f3435e222f9971969ab1331d8c0ca86fab04e
leins275 <feitrich@yandex.ru>
2019-11-14 12:51:25 +0300

    Completed
     - finished report

--- Сеточные методы/lab_1/main.m
+++ Сеточные методы/lab_1/main.m
@@ -3,30 +3,54 @@ problem;
 %%%
 % build a net
 %%%  
-n = 20;
+n = 1280;
 dim = 1;
 errCur = 10;
-
+i = 1;
 while true  
   [v, x, h] = ... 
   FiniteDiffMethod(n, a, b, dim, alpha, beta, gamma, p, q, f);
   
   errPrew = errCur;
   errCur = norm(abs(v - u(x)), h, n);
-  
+  delta(i) = errCur;
+  hi(i) = h;
+  %{
   disp('----------------------------');
   dim
   n
   h
   errCur
+  %}
   
+  dim
+  hi(i)
+  delta(i)
+  
+  i = i + 1;
   n = n * 2;
   if (errCur >= errPrew)
     break;
   end
 end
 
-% plot (x, v, 'linewidth', 2,  x, u(x), 'r', 'linewidth', 2);
+%{
+gf = figure(1);
+hold on;
+grid on;
+axis([min(x), max(x), min(v), max(u(x))]);
+plot(x, v, 'linewidth', 1)
+plot(x, u(x), 'r', 'linewidth', 1);
+%x1 = [1e-5, 1e-3];
+%y1 = [10e-8, 2e-3 + 1e-8 - 2e-5];
+%loglog(hi, delta, 'linewidth', 2);
+%plot(x1, y1, 'k', 'linewidth', 2);
+legend('v', 'u');
+xlabel('x');
+ylabel('y');
+title('O(h^2) approximation (100 nodes)');
+print(gf, "pic/h2_100",'-djpg');
+%}
 
 %{
 figure;
