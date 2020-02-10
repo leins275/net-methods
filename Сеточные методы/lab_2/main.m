5501996258d6d2bc9711ee3e6b761db12ae12fea
LanskovNV <leins275@gmail.com>
2020-02-10 22:48:07 +0300

    Сеточные методы: лаба2
     - в отчёт добавлены описания схем

--- Сеточные методы/lab_2/main.m
+++ Сеточные методы/lab_2/main.m
@@ -3,34 +3,34 @@ clear;
 %  Задание параметров
 a = 0.5; 
 b = 2;
-E = 1e-2; %  1у-3
+E = 1e-3; 
 d = E * 0.1;
-N = 10; %  40000
+N = 80000; 
 
 fprintf('E: %d \n\n', E);
-%while true
+while true
   tic
   fprintf('N: %d \n', N);
   
-  %disp('explicit:');
-  %[x, y, h, t, epsilon] = explicit(a, b, N, d);
+  disp('explicit:');
+  [x, y, h, t, epsilon] = explicit(a, b, N, d);
   %disp('implicit:');
   %[x, y, h, t, epsilon] = implicit(a, b, N, 1);
-  disp('symmetric:');
-  [x, y, h, t, epsilon] = implicit(a, b, N, 0.5);
+  %disp('symmetric:');
+  %[x, y, h, t, epsilon] = implicit(a, b, N, 0.5);
   toc
   
   fprintf('epsilon: %d \n\n', epsilon);
-%  if epsilon <= E
-%    break;
-%  end
-%  N = N * 2;
-%end
+  if epsilon <= E
+    break;
+  end
+  N = N * 2;
+end
 
 %  график
-figure;
-[T, X] = meshgrid(t, x);
-plot3(X, T, y);
+%figure;
+%[T, X] = meshgrid(t, x);
+%plot3(X, T, y);
 
 
 
--- Сеточные методы/lab_2/t_grid.m
+++ Сеточные методы/lab_2/t_grid.m
@@ -4,8 +4,8 @@ function [t, K] = t_grid(ts, N, d, x, h)
   n2 = zeros(N+1, 1);
   while true
     for i=1:N+1
-      n1(i) = abs(u(x(i), (K-1)*ts) - sin(exp(x(i))));
       n2(i) = sin(exp(x(i)));
+      n1(i) = abs(u(x(i), (K-1)*ts) - n2(i));
     end
     if norm2(n1, h) / norm2(n2, h) < d
       break;
